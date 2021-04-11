dfd97e111c1c3ec310483850c04231ebd2cfbfe4
Victor-Aremu <77357316+Victor-Aremu@users.noreply.github.com>
2021-04-11 16:19:30 +0000

    creating Elements

---
+++ js/app.js
@@ -0,0 +1,16 @@
+class Products {
+    constructor(title, price, stock, description, imageUrl){
+    this.title = title;
+    this.price = price;
+    this.stock - stock;
+    this.description = description;
+    this.imageUrl = imageUrl;
+
+    }
+
+}
+
+class Item extends Products{
+
+}
+
---
+++ js/script.js
@@ -0,0 +1,8 @@
+let scaffold = document.getElementById('root');
+let navbar = document.createElement('div');
+navbar.className = 'navbar'
+let container = document.createElement('div');
+let title = document.createElement('h2');
+let price = document.createElement('h4');
+let stock = document.createElement('h4');
+let despription = document.createElement('p');
